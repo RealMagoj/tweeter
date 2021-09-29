67fbc4c99dd7de9f4c0da2e231092545918826a2
RealMagoj <realmagoj@outlook.com>
2021-09-29 01:33:04 -0400

    update composer-char-counter.js logic

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
-  $('.new-tweet').on('keydown', '#tweet-text', function(e) {
+  $('.new-tweet').on('input', '#tweet-text', function(e) {
     $(this).closest('.new-tweet').find('.counter');
     const counter = $(this).closest('.new-tweet').find('.counter');
-    if (e.keyCode === 8 && Number(counter.val()) < 140) {
-      $(counter).html(Number($(counter).val()) + 1);
-    }
-    if (e.keyCode !== 8) {
-      $(counter).html(Number($(counter).val()) - 1);
-    }
+    const current_length = $(this).val().length;
+    $(counter).html(140 - current_length);
     if ($(counter).val() < 0) {
       $(counter).addClass('red');
     } else {
